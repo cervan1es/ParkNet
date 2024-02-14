6d1f6b9c77ac0a1e909eac5c8e9852a40e1d085c
DESKTOP-VAK6RP6\cervantes <cervan1es@gmail.com>
2024-02-14 11:07:26 +0000

    Feature: Add Customer's Page

---
+++ ParkNet_Ricardo.Campos/Pages/Customers/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ParkNet_Ricardo.Campos.Data;
+using ParkNet_Ricardo.Campos.Data.Entities;
+
+namespace ParkNet_Ricardo.Campos.Pages.Customers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ParkNet_Ricardo.Campos.Data.ApplicationDbContext _context;
+
+        public DeleteModel(ParkNet_Ricardo.Campos.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Customer = customer;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer.FindAsync(id);
+            if (customer != null)
+            {
+                Customer = customer;
+                _context.Customer.Remove(Customer);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
