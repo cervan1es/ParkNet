47b68341c8161498fa89e8c348d3a420c290797e
DESKTOP-VAK6RP6\cervantes <cervan1es@gmail.com>
2024-02-20 11:02:22 +0000

    Feature: Add Floor, ParkingSpace and Park Repositories with add async methods

---
+++ ParkNet_Ricardo.Campos/Repositories/ParkRepository.cs
@@ -0,0 +1,22 @@
+﻿using ParkNet_Ricardo.Campos.Data;
+using ParkNet_Ricardo.Campos.Data.Entities;
+
+namespace ParkNet_Ricardo.Campos.Repositories
+{
+    public class ParkRepository(ApplicationDbContext context)
+    {
+        private ApplicationDbContext _context = context;
+
+        public async Task<Park> AddAsyncPark (string parkName, string parkLayout)
+        {
+            var park = new Park
+            {
+                Name = parkName,
+                Layout = parkLayout
+            };
+            _context.Park.Add(park);
+            await _context.SaveChangesAsync();
+            return park;
+        }
+    }
+}
---
+++ ParkNet_Ricardo.Campos/Repositories/ParkingSpaceRepository.cs
@@ -0,0 +1,22 @@
+﻿using ParkNet_Ricardo.Campos.Data;
+using ParkNet_Ricardo.Campos.Data.Entities;
+
+namespace ParkNet_Ricardo.Campos.Repositories
+{
+    public class ParkingSpaceRepository (ApplicationDbContext context)
+    {
+        private ApplicationDbContext _context = context;
+        public async Task<ParkingSpace> AddParkingSpace(Guid floorID, string parkingSpaceCoordenate, char vehicleType)
+        {
+           var parkingSpace = new ParkingSpace
+           {
+               FloorID = floorID,
+               Coordenate = parkingSpaceCoordenate,
+               VehicleType = vehicleType
+           };
+            _context.ParkingSpace.Add(parkingSpace);
+            await _context.SaveChangesAsync();
+            return parkingSpace;
+        }
+    }
+}
