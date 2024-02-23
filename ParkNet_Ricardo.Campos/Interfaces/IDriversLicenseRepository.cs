cd22a170813c726d14f8ffafa5be1e4f4a8f1f07
DESKTOP-VAK6RP6\cervantes <cervan1es@gmail.com>
2024-02-23 12:06:04 +0000

    Feature: Create Interfaces DriversLicenseRepository and DriversLicenseService

---
+++ ParkNet_Ricardo.Campos/Interfaces/IDriversLicenseRepository.cs
@@ -0,0 +1,9 @@
+﻿using ParkNet_Ricardo.Campos.Data.Entities;
+
+namespace ParkNet_Ricardo.Campos.Interfaces
+{
+    public interface IDriversLicenseRepository
+    {
+        DriversLicense Create(Guid customerID, DateTime expireDate, int licenseNumber);
+    }
+}
---
+++ ParkNet_Ricardo.Campos/Interfaces/IDriversLicenseService.cs
@@ -0,0 +1,7 @@
+﻿namespace ParkNet_Ricardo.Campos.Interfaces
+{
+    public interface IDriversLicenseService
+    {
+        bool Create(string customerEmail, DateTime expireDate, int licenseNumber);
+    }
+}
