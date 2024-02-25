1ea8749ecec0cc7a2bfc4fd6c526cdc07625b4d0
DESKTOP-VAK6RP6\cervantes <cervan1es@gmail.com>
2024-02-25 23:58:22 +0000

    Feature: Add Select Parks on CreatePermit

--- ParkNet_Ricardo.Campos/Pages/Permits/Create.cshtml.cs
+++ ParkNet_Ricardo.Campos/Pages/Permits/Create.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using ParkNet_Ricardo.Campos.Data;
 using ParkNet_Ricardo.Campos.Data.Entities;
 
@@ -26,16 +27,25 @@ namespace ParkNet_Ricardo.Campos.Pages.Permits
         public List<CustomerVehicle> Vehicles { get; set; } = new();
         [BindProperty]
         public List<PermitTariff> PermitTariffs { get; set; } = new();
+        [BindProperty]
+        public List<Park> Parks { get; set; } = new();
+        [BindProperty]
+        public Park CurrentPark { get; set; } = default!;
+        [BindProperty]
+        public Guid CurrentParkID { get; set; }
         public Guid CurrentVehicleID { get; set; }
+        public SelectList SelectOptions { get; set; }
         public async Task<IActionResult> OnGetAsync()
         {
             var currentUser = await _userManager.GetUserAsync(User);
             var customer = _context.Customer.FirstOrDefault(c => c.Email == currentUser.Email);
+            Parks = await _context.Park.ToListAsync();
             var vehicles = _context.CustomerVehicle.Where(v => v.CustomerID == customer.ID).ToList();
             if (vehicles.Count == 0) return RedirectToPage("/Vehicle/Create");
             Vehicles = vehicles;
             var permitTariffs = _context.PermitTariff.ToList();
             PermitTariffs = permitTariffs;
+            SelectOptions = new SelectList(_context.Park,nameof(Park.ID),nameof(Park.Name));
             return Page();
         }
 
